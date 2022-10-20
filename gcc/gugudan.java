91f7aef23d810edeba4fb7a00816fa876ae47e06
mchee01 <69677873+mchee01@users.noreply.github.com>
2022-10-20 17:27:41 +0900

    Commit java

--- gcc/gugudan.java
+++ gcc/gugudan.java
@@ -1,15 +1,16 @@
+import java.util.Scanner;
+
 class gugudan{
   public static void main(String[] args) {
+    System.out.print("구구단을 출력하는 프로그램입니다.\n 1~9까지 입력하여주세요. ->");
     Scanner sc = new Scanner(System.in);
     int gugu = sc.nextInt();
     if(gugu==0){
       System.out.println("1~9까지 입력하여 주세요.");
-      return false;
-    }else if(gugu>1&&gugu<9){
-      for(int i = 1;i<=9 i++){
+    } else if(gugu>1&&gugu<9){
+      for(int i = 1;i<=9; i++){
         System.out.println(gugu+"x"+i+"="+(gugu*i));
       }
     }
-
   }
 }
