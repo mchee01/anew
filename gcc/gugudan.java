01f20d5f7f90eb323a43bd46ddb0073bb32a7337
mchee01 <69677873+mchee01@users.noreply.github.com>
2022-10-20 17:20:03 +0900

    commit

--- gcc/gugudan.java
+++ gcc/gugudan.java
@@ -1,7 +1,15 @@
 class gugudan{
   public static void main(String[] args) {
-    for(int i = 0;i<=10){
-
+    Scanner sc = new Scanner(System.in);
+    int gugu = sc.nextInt();
+    if(gugu==0){
+      System.out.println("1~9까지 입력하여 주세요.");
+      return false;
+    }else if(gugu>1&&gugu<9){
+      for(int i = 1;i<=9 i++){
+        System.out.println(gugu+"x"+i+"="+(gugu*i));
+      }
     }
+
   }
 }
