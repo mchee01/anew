bb117c994eb4c97cf9230bf9ee6c5f11b2ea749f
mchee01 <69677873+mchee01@users.noreply.github.com>
2022-10-21 13:50:46 +0900

    change

--- gcc/prime.c
+++ gcc/prime.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 void main() {
   int n;
-  printf("Input Integer=>\n");
+  printf("\nInput Integer=>");
   scanf("%d",&n);
   while (1) {
     if(n==0){
       break;
     }if(checkprime(n) == n){
       printf("%d is a prime number \n",n );
+      break;
     }else{
       printf("%d is not prime number \n",n );
+      break;
     }
   }
 
