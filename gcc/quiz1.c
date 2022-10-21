f0ef9fea09f73577656d48c9a07cb4675421e185
mchee01 <69677873+mchee01@users.noreply.github.com>
2022-10-21 11:47:15 +0900

    commit

--- gcc/quiz1.c
+++ gcc/quiz1.c
@@ -5,11 +5,11 @@ int main() {
   printf("Input num :");
   scanf("%d",&number);
   if(checked(number)==1){
-    printf("%d is odd number~!!",number);
+    printf("%d is odd number~!!\n",number);
   }else if(checked(number)==0){
-    printf("%d is even number~!!",number);
+    printf("%d is even number~!!\n",number);
   }else if(checked(number)!=0||checked(number)!=1){
-    printf("%d Not is number",number);
+    printf("%d Not is number\n",number);
   }
   return 0;
 }
