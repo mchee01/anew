99d067d258f5f97c203bd9d7fa61d5e2f65617cb
mchee01 <69677873+mchee01@users.noreply.github.com>
2022-10-21 14:27:02 +0900

    commit

---
+++ make/test1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "a.h"
+void func1();
+void func2();
+void main(){
+  printf("Test1\n");
+  func1();
+  func2();
+}
---
+++ make/test2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "a.h"
+#include "b.h"
+
+extern void func1(){
+  printf("Test2\n");
+}
