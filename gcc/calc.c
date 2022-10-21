9cdcf1ddd13380621b2bde00459946bc5cc1fabc
mchee01 <69677873+mchee01@users.noreply.github.com>
2022-10-21 10:57:43 +0900

    commit

--- gcc/calc.c
+++ gcc/calc.c
@@ -5,7 +5,7 @@ int main() {
   int x, y;
   printf("Input two numbers : ");
   scanf("%d %d",&x, &y);
-  printf("%d + %d = %d \n", x, y, minus(x, y));
+  printf("%d + %d = %d \n", x, y, plus(x, y));
   printf("%d - %d = %d \n", x, y, minus(x, y));
   return 0;
 }
