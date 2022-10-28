24d7b79354e1698d032aad86b88035854e1d074d
mchee01 <69677873+mchee01@users.noreply.github.com>
2022-10-28 15:57:21 +0900

    testdb

---
+++ db/testdb_stinfo-stgrade_create.sql
@@ -0,0 +1,30 @@
+show databases;
+use testdb;
+show tables;
+
+create table st_info(
+ST_ID int,
+NAME varchar(20),
+DEPT varchar(25)
+) default charset=utf8;
+show tables;
+
+create table st_grade(
+ST_ID int,
+Linux int,
+DB int);
+explain st_info;
+explain st_grade;
+alter table st_info add constraint pk_stinfo primary key(ST_ID);
+alter table st_grade add constraint pk_stinfo primary key(ST_ID);
+
+/*insert 구문*/
+insert into st_info values (202201,'LeeGiilDong','Game');
+insert into st_info values (202202,'KimGiilDong','Computer');
+insert into st_info values (202203,'HongGiilDong','Game');
+
+insert into st_grade values (202201,80,70);
+insert into st_grade values (202202,90,75);
+insert into st_grade values (202203,95,85);
+select * from st_info;
+select * from st_grade;
