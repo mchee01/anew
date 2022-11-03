1ee31f8bb2de9b14343153b6fcda053fd9ed6c82
mchee01 <abc020122@gmail.com>
2022-11-03 17:59:47 +0900

    commit

---
+++ node/hello/hello.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+	res.send("Hello Node JS~!!!")
+})
+
+app.listen(3000, function() {
+	console.log("3000 Port : Server Started~!!");
+})
